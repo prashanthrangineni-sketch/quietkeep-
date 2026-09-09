a2450f8689be552fdbb6ef65a074ba1e3d2b744c
prashanthrangineni-sketch <prashanthrangineni@gmail.com>
2026-09-09 12:10:29 +0530

    Import the endpointing dial

--- src/lib/context/aaria.jsx
+++ src/lib/context/aaria.jsx
@@ -38,6 +38,7 @@ import { useAuth } from '@/lib/context/auth';
 import { useLanguage } from '@/lib/context/language';
 import { routeUtterance, helpText, DESTINATIONS } from '@/lib/aaria-router';
 import { speak, cancelSpeech, setSpeechAuthToken } from '@/components/VoiceTalkback';
+import { endpointSilenceMsFor, MAX_LISTEN_MS } from '@/lib/endpointing';
 import { onWake, initWakeEngine, getWakeWord } from '@/lib/wake-word-engine';
 import { startWebHotword, isWebHotwordEnabled, isHotwordSupported } from '@/lib/aaria-hotword';
 import { checkForNotices } from '@/lib/aaria-watch';
